1d6a0285c8f807696b21543e072acc9475f4feb9
zeerob440 <zeerob440@tutanota.com>
2025-10-30 20:51:41 -0400

    added line 4 about Rico

--- morphing_dog.py
+++ morphing_dog.py
@@ -1,2 +1,4 @@
 #testing GitHub Web and GitHub Deskop updates from one another
 print("Rico is a dog.") #from GitHub Desktop
+# line 2 was recieved at GitHub Web
+print("Rico remains a dog.") # from GitHub Web
